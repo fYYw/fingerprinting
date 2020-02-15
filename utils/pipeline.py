8a30de1d551b275d9069b6dd3d3dcf89e71ba702
fYYw <fyangskip@gmail.com>
2020-02-15 16:04:30 -0600

    config bug

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -20,7 +20,7 @@ class Pipeline(object):
                  device):
         self.data_io = data_io
         self.high_dev_examples, self.low_dev_examples = data_io.build_split_examples(
-            'dev', min_count=self.config['previous_comment_cnt'])
+            'dev', min_count=config['previous_comment_cnt'])
         self.high_dev_iter = data_io.build_iter_idx(self.high_dev_examples)
         self.low_dev_iter = data_io.build_iter_idx(self.low_dev_examples)
         self.test_examples = data_io.build_examples(1., 'test')
